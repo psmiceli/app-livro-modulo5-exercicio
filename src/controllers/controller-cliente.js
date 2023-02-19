a54df38083f9bc67c2d0b44f1726e985f8150058
psmiceli <pablosmiceli@gmail.com>
2023-02-19 03:49:50 -0300

    Aplicando Rotas Post

--- src/controllers/controller-cliente.js
+++ src/controllers/controller-cliente.js
@@ -2,8 +2,13 @@
 //ROTA CLIENTE
 class ControllerCliente {
   static rotas(app) {
+    //rota GET
     app.get('/cliente', (req, res) => {
       res.send("Rota GET do Cliente ativada")
+    })
+    //rota POST
+    app.post('/cliente', (req, res) => {
+      res.send('Rota POST do Cliente ativada')
     })
   }
 }
--- src/controllers/controller-livro.js
+++ src/controllers/controller-livro.js
@@ -2,8 +2,14 @@
 //ROTA LIVRO
 class ControllerLivro {
   static rotas(app) {
+    //rota GET
     app.get('/livro', (req, res) => {
       res.send("Rota GET do Livro ativada")
+    })
+
+    //rota POST
+    app.post('/livro', (req, res) => {
+      res.send('Rota POST do Livro ativada')
     })
   }
 }
